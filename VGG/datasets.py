264521ce39c37a7b6a363abf212b310698e24909
YeoulLee <51706773+YeoulLee@users.noreply.github.com>
2020-07-07 23:59:42 +0900

    Update datasets.py

--- VGG/datasets.py
+++ VGG/datasets.py
@@ -1,3 +1,13 @@
 ### torch lib
 import torch
-import torch.utils.data as data
+from torch.utils.data import Dataset
+
+# Download the data from https://download.pytorch.org/tutorial/hymenoptera_data.zip
+
+class HymenopteraDataset(Dataset):
+  def __init__(self,):
+    pass
+  def __len__(self):
+    pass
+  def __getitem__(self,idx):
+    pass
