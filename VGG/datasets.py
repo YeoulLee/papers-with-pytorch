5b74f64e03cdf93cf712f9bfc6aab3aaee1d5103
YeoulLee <51706773+YeoulLee@users.noreply.github.com>
2020-07-08 23:37:26 +0900

    Update datasets.py

--- VGG/datasets.py
+++ VGG/datasets.py
@@ -2,12 +2,19 @@
 import torch
 from torch.utils.data import Dataset
 
+from glob import glob
+from PIL import Image
+
 # Download the data from https://download.pytorch.org/tutorial/hymenoptera_data.zip
 
 class HymenopteraDataset(Dataset):
-  def __init__(self,):
-    pass
-  def __len__(self):
-    pass
-  def __getitem__(self,idx):
-    pass
+    def __init__(self, root_dir, size=(224,224)):
+        self.files = glob(root_dir)
+    
+    def __len__(self):
+        return len(self.files)
+  
+    def __getitem__(self,idx):
+        img = np.asarray(Image.open(self.files[idx]).resize(self.size))
+        label = self.files[idx].split('/')[-2]
+        return img, label
