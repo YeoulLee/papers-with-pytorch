668b44c6f29ecc361aa7f214217ac65c049ea81e
YeoulLee <51706773+YeoulLee@users.noreply.github.com>
2020-07-22 00:18:33 +0900

    Update train.py

--- VGG/train.py
+++ VGG/train.py
@@ -1,13 +1,149 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
-from torchvision import transforms
-import datasets
+from torchvision import datasets, models, transforms
+import os
+import time
+import copy
 
 ### 1. Data Pipeline
-mean = [0.485, 0.456, 0.406]
-std = [0.229, 0.224, 0.225]
-normalize = transforms.Normalize(mean=mean, std=std)
-transform = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor(), normalize])
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
 
-trainset = datasets.HymenopteraDataset(root=args.data_root + '/train', transform=transform)
-trainloader = DataLoader(trainset, batch_size=args.train_batch_size, shuffle=True)
+data_dir = 'data/hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+#for image, label in image_datasets['train']:
+#    print(image.shape, label)
+
+#for images, labels in dataloader['train']:
+#    print(images.shape, labels)
+
+#import utils
+#utils.imshow(image_datasets['train'][50][0])
+
+
+### 2. Network architecture
+vgg = models.vgg16(pretrained=True)
+
+for param in vgg.features.parameters():
+    param.requires_grad = False
+    
+num_in_features = vgg.classifier[6].in_features
+vgg.classifier[6] = nn.Linear(num_in_features, 2)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model_transfer = vgg.to(device)
+
+#print("Model's state_dict:")
+#for param_tensor in model_transfer.state_dict():
+#    print(param_tensor, "\t", model_transfer.state_dict()[param_tensor].size())
+
+
+### 3. Evaluating using loss function
+criterion = nn.CrossEntropyLoss()
+
+
+### 4. Optimizer
+optimizer_transfer = optim.SGD(model_transfer.parameters(), lr=0.001, momentum=0.9)
+
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_transfer, step_size=7, gamma=0.1)
+
+#print("Optimizer's state_dict:")
+#for var_name in optimizer_transfer.state_dict():
+#    print(var_name, "\t", optimizer_transfer.state_dict()[var_name])
+
+
+### train
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+ 
+model_transter = train_model(model_transfer, criterion, optimizer_transfer, exp_lr_scheduler, num_epochs=25)
+
+  
